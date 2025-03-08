ddb8af0da065ffbff269e1b7f1fd31cf9a3f7979
Ekaterina Vostrikova <ekavos@hotmail.com>
2025-03-08 23:54:21 +0100

    Create: inicial project structure

---
+++ resources/js/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-app>
+        <v-app-bar title="Gestión de tareas" class="text-center">jfkelsjl
+        </v-app-bar>
+    </v-app>
+</template>
+
+<script>
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="<Ekaterina Vostrikova>">
+        <title>Gestor de tareas</title>
+
+        @vite(['resources/js/app.js'])
+    </head>
+<body>
+<div id="app"></div>
+</body>
+
+</html>
