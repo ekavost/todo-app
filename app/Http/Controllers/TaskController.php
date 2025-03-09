f7dcd180f725e9ea107952d20cf57789a156fb79
Ekaterina Vostrikova <ekavos@hotmail.com>
2025-03-09 01:20:42 +0100

    Add: task creation (inicial test version)
    Introduce a `CreateTaskRequest` for validation during task creation. Update `TaskController` to handle task creation and retrieval. Enhance the frontend with a task form and axios integration for posting tasks.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTaskRequest;
+use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\createRequest;
 
 class TaskController extends Controller
 {
-    //
+    public function create(CreateTaskRequest $request) {
+        $data = $request->validated();
+        $title = Task::create($data);
+        return $title;
+    }
+    public function show()
+    {
+        $data = Task::all();
+        return $data;
+    }
+    public function edit(){
+
+    }
+    public function delete()
+    {
+
+    }
 }
--- resources/js/components/TaskForm.vue
+++ resources/js/components/TaskForm.vue
@@ -1,3 +1,29 @@
 <template>
-
+    <v-form>
+        <v-text-field label="Tarea" v-model="title"></v-text-field>
+        <v-textarea label="Descripción" v-model="description"></v-textarea>
+        <v-btn class="me-4" type="button" @click.prevent="createTask"> Crear </v-btn>
+    </v-form>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            title:'',
+            description:'',
+            completed:false,
+        }
+    },
+    methods: {
+        createTask(){
+            console.log(this.title);
+            axios.post('/', {
+                title: this.title,
+                description: this.description
+            }).then(response => {console.log(response.data)})
+        }
+    }
+}
+
+</script>
